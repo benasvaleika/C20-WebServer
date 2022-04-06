ee0d51e1c082d484488471af8b33e84be6d93499
benasvaleika <bentini113@gmail.com>
2022-04-07 01:59:49 +0300

    complete

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,13 +11,15 @@ fn main() {
 
     let thread_pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         thread_pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.")
 }
 
 fn handle_connection(mut stream: TcpStream) {
